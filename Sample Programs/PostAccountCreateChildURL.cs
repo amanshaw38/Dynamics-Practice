043fbfe28ca8b9ed2573d7e9c4eb88f0447926bd
Aman Shaw <amanshaw38@gmail.com>
2020-04-23 20:10:35 +0530

    First Commit

---
+++ Sample Programs/PostAccountCreateChildURL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+namespace Sample_Programs
+{
+    public class PostAccountCreateChildURL : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
